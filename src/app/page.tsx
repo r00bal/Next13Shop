0106c5d35fdcb072add36c77f7fae522cfad2012
r00bal <andrzejewski.piotr@gmail.com>
2023-09-24 17:35:51 +0200

    Add font styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 					priority
 				/>
 			</div>
-			<h1> VERCEL DEPLOYING TEST</h1>
+			<h1 className="text-4xl font-semibold"> VERCEL DEPLOYING TEST</h1>
 			<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
 				<a
 					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
