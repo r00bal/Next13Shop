0e47e8924ebfbbeeed8c511a6d696a50c34a7b46
r00bal <andrzejewski.piotr@gmail.com>
2024-06-26 00:09:02 +0200

    feat: refactor types

--- src/ui/atoms/Spinner.tsx
+++ src/ui/atoms/Spinner.tsx
@@ -8,7 +8,7 @@ type SpinnerProps = {
 export const Spinner = ({ size = 24, color = "gray" }: SpinnerProps) => {
 	return (
 		<div className="flex items-center justify-center">
-			{/* {@ts-ignore} */}
+			{/* @ts-ignore */}
 			<FaSpinner className={`animate-spin text-${color}-500`} size={size} />
 		</div>
 	);
