17cc75c2259df3bc6ff6724b38f6b537a362eaba
r00bal <andrzejewski.piotr@gmail.com>
2024-07-01 22:42:33 +0200

    feat: add avrageRating

--- src/ui/molecules/ReviewForm.tsx
+++ src/ui/molecules/ReviewForm.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Input } from "../atoms/Input";
 import { TextArea } from "../atoms/TextArea";
-import { RatingStarsDisplay } from "./RatingStarsDisplay";
 import { RatingStarsInput } from "./RatingStarsInput";
 import { type ReviewsType } from "@/api/reviews";
 import { addReviewsAction } from "@/app/product/actions";
@@ -16,57 +15,33 @@ export const ReviewForm = ({
 }) => {
 	const [resetFormIndex, setResetFormIndex] = useState(0);
 	return (
-		<div className="lg:col-span-4">
-			{/* TODO Move this description reviews content to the revies compoennt */}
-			<h2 className="text-2xl font-bold tracking-tight text-gray-900">
-				Customer Reviews
-			</h2>
-			<div className="mt-3 flex items-center">
-				{/* TODO Add avrage rating */}
-				<div title="3.6 out of 5 stars">
-					<RatingStarsDisplay rating={4} />
-					<p className="sr-only">3.6 out of 5 stars</p>
-				</div>
-
-				<p className="ml-2 text-sm text-gray-900">Based on 292 reviews</p>
-			</div>
-			<div className="mt-6">
-				<h3 className="sr-only">Review data</h3>
-			</div>
-			<div className="mt-10">
-				<h3 className="text-lg font-medium text-gray-900">
-					Share your thoughts
-				</h3>
-				<p className="mt-1 text-sm text-gray-600">
-					If you’ve used this product, share your thoughts with other customers
-				</p>
-				<form
-					key={resetFormIndex}
-					action={async (formaData: FormData) => {
-						const newReviewFormData = getReviewFormData(formaData);
-						// TODO: implement form validation
-						if (!newReviewFormData) return;
-						setResetFormIndex((state) => state + 1);
-						onAddOptimisticReview(createNewReview(newReviewFormData));
-						await addReviewsAction(formaData);
-					}}
-					data-testid="add-review-form"
-					className="mt-2 flex flex-col gap-y-2"
+		<>
+			<form
+				key={resetFormIndex}
+				action={async (formaData: FormData) => {
+					const newReviewFormData = getReviewFormData(formaData);
+					// TODO: implement form validation
+					if (!newReviewFormData) return;
+					setResetFormIndex((state) => state + 1);
+					onAddOptimisticReview(createNewReview(newReviewFormData));
+					await addReviewsAction(formaData);
+				}}
+				data-testid="add-review-form"
+				className="mt-2 flex flex-col gap-y-2"
+			>
+				<input type="hidden" value={productId} name="productId" />
+				<Input title="Review title" name="headline" />
+				<TextArea label="Review content" name="content" />
+				<RatingStarsInput label="Rating" name="rating" />
+				<Input title="Name" name="name" />
+				<Input title="Email" name="email" type="email" />
+				<button
+					type="submit"
+					className="mt-6 inline-flex w-full items-center justify-center rounded-md bg-gray-900 px-8 py-2 text-sm font-medium text-gray-50 hover:bg-gray-700 focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50"
 				>
-					<input type="hidden" value={productId} name="productId" />
-					<Input title="Review title" name="headline" />
-					<TextArea label="Review content" name="content" />
-					<RatingStarsInput label="Rating" name="rating" />
-					<Input title="Name" name="name" />
-					<Input title="Email" name="email" type="email" />
-					<button
-						type="submit"
-						className="mt-6 inline-flex w-full items-center justify-center rounded-md bg-gray-900 px-8 py-2 text-sm font-medium text-gray-50 hover:bg-gray-700 focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-					>
-						Submit review
-					</button>
-				</form>
-			</div>
-		</div>
+					Submit review
+				</button>
+			</form>
+		</>
 	);
 };
--- src/ui/organisms/Reviews.tsx
+++ src/ui/organisms/Reviews.tsx
@@ -3,6 +3,7 @@ import { useOptimistic } from "react";
 
 import { ReviewForm } from "../molecules/ReviewForm";
 import { ReviewsList } from "../molecules/ReviewsList";
+import { RatingStarsDisplay } from "../molecules/RatingStarsDisplay";
 import { type ReviewsType } from "@/api/reviews";
 
 export type ReviewsProps = {
@@ -17,13 +18,45 @@ export const Reviews = ({ productId, reviews }: ReviewsProps) => {
 	>(reviews, (state, newReview) => {
 		return [...state, newReview];
 	});
+	const ratingsCount = reviews.length;
+	const avrageRating =
+		reviews.reduce((acc, next) => acc + next.rating, 0) / ratingsCount;
+	const ratingTitle = `${avrageRating} out of 5 stars`;
+	const basedOnReviews = `Based on ${ratingsCount} review${
+		ratingsCount > 1 ? "s" : ""
+	}`;
 
 	return (
 		<div className="mx-auto max-w-2xl lg:grid lg:max-w-7xl lg:grid-cols-12 lg:gap-x-8 lg:py-16">
-			<ReviewForm
-				productId={productId}
-				onAddOptimisticReview={addOptimisticReview}
-			/>
+			<div className="lg:col-span-4">
+				<h2 className="text-2xl font-bold tracking-tight text-gray-900">
+					Customer Reviews
+				</h2>
+				<div className="mt-3 flex items-center">
+					<div title={ratingTitle}>
+						<RatingStarsDisplay rating={avrageRating} />
+						<p className="sr-only">{ratingTitle}</p>
+					</div>
+
+					<p className="ml-2 text-sm text-gray-900">{basedOnReviews}</p>
+				</div>
+				<div className="mt-6">
+					<h3 className="sr-only">Review data</h3>
+				</div>
+				<div className="mt-10">
+					<h3 className="text-lg font-medium text-gray-900">
+						Share your thoughts
+					</h3>
+					<p className="mt-1 text-sm text-gray-600">
+						If you’ve used this product, share your thoughts with other
+						customers
+					</p>
+					<ReviewForm
+						productId={productId}
+						onAddOptimisticReview={addOptimisticReview}
+					/>
+				</div>
+			</div>
 			<ReviewsList reviews={optimisticReviews} />
 		</div>
 	);
